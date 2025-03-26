5fd8ff02b209703929d7486dfed8564800073954
TLWhite <TLWhite@me.com>
2025-03-26 17:33:08 -0500

    Added Elevator subsystem structure  and some Autos

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -45,17 +45,17 @@ public class DriveConstants {
     public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
 
     // Device CAN IDs
-    public static final int pigeonCanId = 9;
+    public static final int pigeonCanId = 10;
 
-    public static final int frontLeftDriveCanId = 1;
-    public static final int backLeftDriveCanId = 3;
-    public static final int frontRightDriveCanId = 5;
-    public static final int backRightDriveCanId = 7;
+    public static final int frontLeftDriveCanId = 4;
+    public static final int backLeftDriveCanId = 6;
+    public static final int frontRightDriveCanId = 2;
+    public static final int backRightDriveCanId = 8;
 
-    public static final int frontLeftTurnCanId = 2;
-    public static final int backLeftTurnCanId = 4;
-    public static final int frontRightTurnCanId = 6;
-    public static final int backRightTurnCanId = 8;
+    public static final int frontLeftTurnCanId = 5;
+    public static final int backLeftTurnCanId = 7;
+    public static final int frontRightTurnCanId = 3;
+    public static final int backRightTurnCanId = 9;
 
     // Drive motor configuration
     public static final int driveMotorCurrentLimit = 60;
---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.elevator;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
+ * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the constants are needed, to
+ * reduce verbosity.
+ */
+public final class ElevatorConstants {
+    public static final int L1 = 5;
+    public static final int L2 = 10;
+    public static final int L3 = 15;
+    public static final int L4 = 20;
+
+    public static final int downPos = 0;
+    public static final int bottomPos = 0;
+
+    public static final double minPos = 0;
+    public static final double maxPos = 10;
+
+    public static final int countsPerInch = 0;
+    public static final int posTolerance = 0;
+
+    public static final double kP = 0.0;
+    public static final double kD = 0.0;
+    public static final double kI = 0.0;
+    public static final double kS = 0.0;
+    public static final double kG = 0.0;
+    public static final double kV = 0.0;
+
+    public static final int max_output = 0;
+
+    public static final int elevatorLCanId = 11;
+    public static final int elevatorRCanId = 12;
+
+    public static final int maxVelocity = 0;
+    public static final int maxAcceleration = 0;
+
+    public static final int limitSwitchPort = 0;
+}
