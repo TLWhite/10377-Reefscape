e69c83957e0b21d7f83e064a7846b6b1068cefef
TLWhite <TLWhite@me.com>
2025-03-29 07:58:20 -0500

    small automatic change to buildconstants file

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "10377-Reefscape";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 8;
-    public static final String GIT_SHA = "b45d888107f40a29ac289a6e62fafa8c43adde15";
-    public static final String GIT_DATE = "2025-03-29 03:59:07 EDT";
-    public static final String GIT_BRANCH = "Elevator";
-    public static final String BUILD_DATE = "2025-03-29 08:47:33 EDT";
-    public static final long BUILD_UNIX_TIME = 1743252453734L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 14;
+    public static final String GIT_SHA = "b747929a21d05f2d97ac472fca15093f53fd05b1";
+    public static final String GIT_DATE = "2025-03-29 08:56:56 EDT";
+    public static final String GIT_BRANCH = "master";
+    public static final String BUILD_DATE = "2025-03-29 08:57:39 EDT";
+    public static final long BUILD_UNIX_TIME = 1743253059599L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
